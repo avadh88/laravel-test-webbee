62c24d91b9d156dcb5e255288b4df41631173845
avadh <avadh@gmail.com>
2021-05-02 17:20:11 +0530

    set relation ship with database

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,7 +4,22 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Workshop;
 class Event extends Model
 {
+    protected $primaryKey = 'id';
+
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function workshop()
+    {
+        return $this->hasMany(Workshop::class,'event_id');
+    }
 }
